6f9782a8cdc4658c9db2b00dd4185270a4ace71d
oscar20 <oscar_almazan@outlook.com>
2018-05-11 16:44:37 -0500

    Creación de proyecto

---
+++ webServices/FlickrAPI.swift
@@ -0,0 +1,12 @@
+//
+//  FlickrAPI.swift
+//  webServices
+//
+//  Created by d182_oscar_a on 11/05/18.
+//  Copyright © 2018 d182_oscar_a. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrAPI {
+}
---
+++ webServices/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  webServices
+//
+//  Created by d182_oscar_a on 11/05/18.
+//  Copyright © 2018 d182_oscar_a. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+   
+    @IBOutlet var imageView: UIImageView!
+
+}
