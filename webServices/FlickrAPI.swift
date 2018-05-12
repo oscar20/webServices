fc9de1e55aaad75074bf76470488890a2785d3d4
oscar20 <oscar_almazan@outlook.com>
2018-05-11 19:34:50 -0500

    Peticion exitosa..

--- webServices/FlickrAPI.swift
+++ webServices/FlickrAPI.swift
@@ -17,8 +17,7 @@ struct FlickrAPI {
     private static let baseURLString = "https://api.flickr.com/services/rest"
     private static let apiKey = "a6d819499131071f158fd740860a5a88"
     
-    private static func flickrURL(method: Method,
-                                  parameters: [String:String]?) -> URL {
+    private static func flickrURL(method: Method,parameters: [String:String]?) -> URL {
         //return URL(string: "")!
         
         var components = URLComponents(string: baseURLString)!
